206607a666c652d5d8a9419a23e485bc373dbd1c
Raúl <raul.hernandez@testeandosoftware.com>
2019-08-30 23:53:23 +0200

    Initial commit
    Updated README.md and .gitignore.
    Basic infrastructure for testing (including integration with travis and sonarcloud).
    First failing test.

---
+++ test/martianRobots.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('martianRobots', function () {
+    it('true is true', function () {
+        expect(true).to.equal(false);
+    });
+});
