2bedafb18f8892ddaa976c39f6ae5da1a652f365
Raúl <raul.hernandez@testeandosoftware.com>
2019-09-01 23:17:26 +0200

    Mars grid size verification
    Mars grid size verification

--- src/martianRobots.js
+++ src/martianRobots.js
@@ -7,6 +7,19 @@ var processInput = function (inputString) {
         throw new Error('Input string cannot be empty');
     }
     else {
+        const robotInstructions = inputString.split("\n");
+        const marsSize = robotInstructions[0].split(" ");
+        const gridMaxWidth = marsSize[0];
+        const gridMaxHeight = marsSize[1];
+        return verifyMarsSize(gridMaxWidth, gridMaxHeight);
+    }
+}
+
+var verifyMarsSize = function (gridMaxWidth, gridMaxHeight) {
+    if (gridMaxWidth > 50 || gridMaxWidth < 0 || gridMaxHeight > 50 || gridMaxHeight < 0) {
+        throw new Error('Incorrect upper-right coordinates');
+    }
+    else {
         return('1 1 E\n3 3 N LOST\n2 3 S');
     }
 }
