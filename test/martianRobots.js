74c108800311637a03f1cac2f2fafbce0bc63800
Raúl <raul.hernandez@testeandosoftware.com>
2019-09-01 17:17:48 +0200

    Retruns the right output for the input given in the exercise
    Retruns the right output for the input given in the exercise

--- test/martianRobots.js
+++ test/martianRobots.js
@@ -4,7 +4,11 @@ var martianRobots = require('./../src/martianRobots.js');
 
 describe('Given a new input', function () {
 
-    it('with an empty string passed in', function() {
-        expect (function() {martianRobots('')}).to.throw(Error,  /empty/);
-      });
+    it('with an empty string passed in', function () {
+        expect(function () { martianRobots('') }).to.throw(Error, /empty/);
+    });
+
+    it('with the right data passed in', function () {
+        expect(martianRobots('5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL')).to.equal('1 1 E\n3 3 N LOST\n2 3 S');
+    });
 });
