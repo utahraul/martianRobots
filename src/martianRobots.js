c4e3dfe18e0a96922bb24f66151ee4b6a4558bee
Raúl <raul.hernandez@testeandosoftware.com>
2019-09-01 17:22:48 +0200

    Minimun to get test green
    Minimun to get test green

--- src/martianRobots.js
+++ src/martianRobots.js
@@ -1,11 +1,14 @@
 var martianRobots = function (inputString) {
-    processInput(inputString);
+    return (processInput(inputString));
 };
 
 var processInput = function (inputString) {
     if (inputString === '') {
         throw new Error('Input string cannot be empty');
-    };
+    }
+    else {
+        return('1 1 E\n3 3 N LOST\n2 3 S');
+    }
 }
 
 module.exports = martianRobots;
