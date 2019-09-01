d851e9fa6abeac5c8ff22539938b9acc4cebf6fc
Raúl <raul.hernandez@testeandosoftware.com>
2019-09-01 23:02:32 +0200

    Tests to verify upper-right coordinates
    Tests to verify upper-right coordinates

--- test/martianRobots.js
+++ test/martianRobots.js
@@ -8,6 +8,22 @@ describe('Given a new input', function () {
         expect(function () { martianRobots('') }).to.throw(Error, /empty/);
     });
 
+    it('with an incorrect upper-right coordinates string passed in. x > 50', function () {
+        expect(function () { martianRobots('51 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
+    });
+
+    it('with an incorrect upper-right coordinates string passed in. x < 0', function () {
+        expect(function () { martianRobots('-1 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
+    });
+
+    it('with an incorrect upper-right coordinates string passed in. y > 50', function () {
+        expect(function () { martianRobots('1 51\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
+    });
+
+    it('with an incorrect upper-right coordinates string passed in. y < 0', function () {
+        expect(function () { martianRobots('1 -1\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
+    });
+
     it('with the right data passed in', function () {
         expect(martianRobots('5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL')).to.equal('1 1 E\n3 3 N LOST\n2 3 S');
     });
