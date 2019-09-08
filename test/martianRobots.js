4a7d7a450f5ff84afb8ecc6ca1075671e96d4d9b
Raúl <raul.hernandez@testeandosoftware.com>
2019-09-08 17:19:52 +0200

    Minor changes in test cases
    Minor changes in test cases

--- test/martianRobots.js
+++ test/martianRobots.js
@@ -5,32 +5,32 @@ var martianRobots = require('./../src/martianRobots.js');
 describe('Given a new input ', function () {
 
     describe('with an empty string passed in ', function () {
-        it('should throw and error', function () {
+        it('should throw an error', function () {
             expect(function () { martianRobots('') }).to.throw(Error, /empty/);
         });
     });
 
     describe('with an incorrect upper-right coordinates string passed in ', function () {
-        it('should throw and error when x > 50', function () {
+        it('should throw an error when x > 50', function () {
             expect(function () { martianRobots('51 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
         });
 
-        it('should throw and error when x < 0', function () {
+        it('should throw an error when x < 0', function () {
             expect(function () { martianRobots('-1 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
         });
 
-        it('should throw and error when y > 50', function () {
+        it('should throw an error when y > 50', function () {
             expect(function () { martianRobots('1 51\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
         });
 
-        it('should throw and error when y < 0', function () {
+        it('should throw an error when y < 0', function () {
             expect(function () { martianRobots('1 -1\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL') }).to.throw(Error, 'Incorrect upper-right coordinates');
         });
     });
 
     describe('with one robot and one movement data passed in ', function () {
         it('should return 1 2 E', function () {
-            expect(martianRobots('5 5\n1 1 E\nF')).to.equal('1 2 E');
+            expect(martianRobots('5 5\n1 1 E\nF')).to.equal('2 1 E\n');
         });
     });
 
