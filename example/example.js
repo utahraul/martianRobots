d3573ba3f98872d2f48f731e1c4a52d6cc9029c2
Raúl <raul.hernandez@testeandosoftware.com>
2019-09-10 17:29:42 +0200

    Added instructions to modify the example
    Added instructions to modify the example

--- example/example.js
+++ example/example.js
@@ -3,4 +3,5 @@ var martianRobots = require('./../src/martianRobots.js');
 var exampleData = '5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 2 W\nLLFFFLFLFL';
 
 let result = martianRobots(exampleData);
+console.log('Running example.\n\nThe input data can be modified by manually editing the file example.js and changing the string in exampleData (Line 3) for the desired data.\n')
 console.log('Input: \n' + exampleData + '\n\n' + 'Output:\n' + result);
